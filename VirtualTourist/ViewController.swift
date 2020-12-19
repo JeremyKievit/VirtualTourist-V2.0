9d2e776bf625fa7fcb6ce660680fc3d468ce2e21
Jeremy Kievit <jeremyjkievit@gmail.com>
2020-12-19 13:32:31 -0800

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by admin on 12/19/20.
+//  Copyright © 2020 Com.JeremyKievit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
